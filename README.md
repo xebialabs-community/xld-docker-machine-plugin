1ab3adf03bbf57a0389c50e801443a627c2aff24
Benoit Moussaud <bmoussaud@xebialabs.com>
2017-02-09 16:51:26 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Overview #
+
+The Docker Machine plugin is a XL Deploy plugin.
+It adds the capability for provision `docker.Engine' using Docker Machines.
+
+# CI status #
+
+[![Build Status][xld-docker-plugin-travis-image]][xld-docker-machine-plugin-travis-url]
+
+[xld-docker-machine-plugin-travis-image]: https://travis-ci.org/xebialabs-community/xld-docker-machine-plugin.svg?branch=master
+[xld-docker-machine-plugin-travis-url]: https://travis-ci.org/xebialabs-community/xld-docker-machine-plugin
+
+
+# Installation #
+
+Place the plugin JAR file into your `SERVER_HOME/plugins` directory.
+Dependencies:
+
+* XL Deploy 6.0+
+* XL Deploy Docker plugin 6.1.0+
+* OvertherePy 0.3+ https://github.com/xebialabs-community/overthere-pylib/releases/tag/v0.0.3
+
---
+++ src/main/resources/docker-machine/destroy.py
@@ -0,0 +1,28 @@
+#
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+# FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+#
+
+from overtherepy import LocalConnectionOptions, OverthereHost, OverthereHostSession
+from com.xebialabs.overthere import OperatingSystemFamily
+
+
+print "Removing docker '{0}' machine ".format(machine_name)
+command_line = "docker-machine rm --force {0}".format(machine_name)
+
+localOpts = LocalConnectionOptions(os=OperatingSystemFamily.UNIX)
+host = OverthereHost(localOpts)
+session = OverthereHostSession(host)
+
+print "Executing '{0}'....".format(command_line)
+response = session.execute(command_line)
+for s in response.stdout:
+    print s
+for s in response.stderr:
+    print s
+
+session.close_conn()
+
+if response.rc != 0:
+    sys.exit(rc)
