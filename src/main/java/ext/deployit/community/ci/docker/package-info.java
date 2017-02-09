b48011e8e8677b5613fd4d4516b1e9cd19513dbb
Benoit Moussaud <bmoussaud@xebialabs.com>
2017-02-09 16:53:24 +0100

    move to docker.machine

--- src/main/java/ext/deployit/community/ci/docker/package-info.java
+++ src/main/java/ext/deployit/community/ci/docker/package-info.java
@@ -3,7 +3,7 @@
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
  * FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
  */
-@Prefix("docker")
+@Prefix("docker.machine")
 package ext.deployit.community.ci.docker;
 
 import com.xebialabs.deployit.plugin.api.udm.Prefix;
